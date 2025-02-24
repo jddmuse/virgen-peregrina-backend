a7892e2c4165447b2fd2493e3f3453ecf235d533
Jeison Damian <damianm09.01@gmail.com>
2025-02-24 16:06:45 -0500

    Ajuste en LoginRequest

--- src/main/kotlin/com/virgen/peregrina/demo/data/request/LoginRequest.kt
+++ src/main/kotlin/com/virgen/peregrina/demo/data/request/LoginRequest.kt
@@ -1,6 +1,8 @@
 package com.virgen.peregrina.demo.data.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class LoginRequest(
-    val email: String,
-    val pass: String
+    @JsonProperty("email") val email: String,
+    @JsonProperty("pass") val pass: String
 )
